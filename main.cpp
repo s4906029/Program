4473bdf52b7544cde094a415053d2a309e1e5657
s4906029 <39380204+s4906029@users.noreply.github.com>
2018-05-17 17:27:08 +0100

    src/

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "MainWindow.h"
+#include <QApplication>
+
+#include <iostream>
+
+int main( int argc, char **argv )
+
+{
+
+    QApplication a( argc, argv );
+
+    MainWindow w;
+
+    w.show();
+
+    return a.exec();
+
+}
